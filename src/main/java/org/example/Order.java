a594d86bab16ff83535da315f206c6274cbe0b00
Vludisluv <140197616+Vludisluv7@users.noreply.github.com>
2023-10-27 16:04:56 +0300

    Update Order.java

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,8 +1,14 @@
 package org.example;
 
 public class Order {
-    public String toString;
+    private String description; // Поле для описания заказа
 
-    public Order(String s) {
+    public Order(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
     }
 }
